cffa0573d3d9ab837a5d9c10fbf208d096595cd6
Warut Surapat <s.warut@gmail.com>
2021-12-02 09:47:00 +0900

    Solve day 1-2.

--- lib/day1.ex
+++ lib/day1.ex
@@ -11,13 +11,30 @@ defmodule Day1 do
     process_part1(input, 0)
   end
 
-  def process_part1([a| [b | _c] = x], count) when b > a do
+  def process_part1([a | [b | _c] = x], count) when b > a do
     process_part1(x, count + 1)
   end
-  def process_part1([a| [b | _c] = x], count) when b <= a do
+  def process_part1([a | [b | _c] = x], count) when b <= a do
     process_part1(x, count)
   end
   def process_part1([_x], count) do
     IO.puts("increment = #{count}")
+  end
+
+  def solve_part2 do
+    input = get_input()
+    process_part2(input, 3, 0)
+  end
+
+  def process_part2(x, window, count) when length(x) <= window do
+    IO.puts("increment = #{count}")
+  end
+  def process_part2([a | b] = x, window, count) do
+    first_sum = x |> Enum.take(window) |> Enum.sum()
+    second_sum = (x |> Enum.take(window + 1) |> Enum.sum()) - a
+    case second_sum > first_sum do
+      true -> process_part2(b, window, count + 1)
+      _ ->  process_part2(b, window, count)
+    end
   end
 end
